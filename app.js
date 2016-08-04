2fd1ffd44aa856d54d897af74c32a2e41594b495
Aaron Perez <db.robot@yahoo.com>
2016-08-03 23:08:57 -0400

    TMFNode: minor improvements
    * jshint
    * swagger ui to serve up json @ /api-docs
    * swagger-tools

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var SwaggerRestify = require('swagger-restify-mw');
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
 var restify = require('restify');
 var app = restify.createServer();
 
@@ -13,7 +14,26 @@ var config = {
 SwaggerRestify.create(config, function(err, swaggerRestify) {
   if (err) { throw err; }
 
+  // Add swagger-ui with /api-docs
+  app.use(SwaggerUi(swaggerRestify.runner.swagger));
+
   swaggerRestify.register(app);
+
+  // Custom error handler that returns JSON
+  app.use(function(err, req, res, next) {
+    if (typeof err !== 'object') {
+      // If the object is not an Error, create a representation that appears to be
+      err = {
+        message: String(err) // Coerce to string
+      };
+    } else {
+      // Ensure that err.message is enumerable (It is not by default)
+      Object.defineProperty(err, 'message', { enumerable: true });
+    }
+    // Return a JSON representation of #/definitions/ErrorResponse
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(err));
+  });
 
   var port = process.env.PORT || 10010;
   app.listen(port);
